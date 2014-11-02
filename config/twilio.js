a8f433638254fc4a03d228934672cab936075f71
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-11-02 15:10:31 -0500

    Complete twilio interfacing in worker.js

--- config/twilio.js
+++ config/twilio.js
@@ -1,7 +1,12 @@
 // May want to increase the security of these components in the future
+var testSid = 'AC2c1eac8fa16d00d2fd7b7a62b8fb181b';
+var testToken = '36fb69e0fdc6403be87bcdac8faf64f3';
+
 var accountSid = 'AC30ea0b2e26c3293997fbf599981fd28c';
 var authToken = '0972510307bc4eb384fc54dd4fb2fe2c';
-var client = require('twilio')(accountSid, authToken);
+
+// var client = require('twilio')(accountSid, authToken);
+var client = require('twilio')(testSid, testToken);
 
 module.exports = {
   messages : client.messages
--- worker.js
+++ worker.js
@@ -15,28 +15,35 @@ var twilio = require('./config/twilio.js');
 User.find().exec(function(error, users, done) {
 
   if (error) {
-    return done(error);
+    return console.log(error);
   }
 
   for (var i = 0; i < users.length; i++) {
 
     var currentTime = new Date();
     console.log(currentTime.getHours());
+    // heroku server is on GMT, ie: +5 hours from EST
 
-    var message = "Hey mukie, guess what? ... You're hot!";
+    var affirmation = "Hey mukie, guess what? ... You're hot!";
 
-    // twilio.messages.create({
-    //   body: message,
-    //   from: "+13239995226",
-    //   to: users[i].local.phone
-    // }, function(err, message) {
-    //   console.log(message);
-    // });
+    twilio.messages.create({
+      body: affirmation,
+      // from: "+13239995226",
+      from: "+15005550006",
+      to: users[i].local.phone
+    }, function(error, message) {
+
+      if (!error) {
+        console.log('Message sent on: ' + message.dateCreated + ' to ' + message.to);
+      }
+      else {
+        console.log('There was an error with the Twilio client. Ugh whatd you do.')
+      }
+
+    });
 
   }
 
-  done(process.exit());
+  mongoose.disconnect();
 
 });
-
-console.log('Scheduler successfully ran :]');
